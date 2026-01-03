e160030207321a4019d79bf3d5eba6fb1cb7d22f
kunalkhare21 <151828536+kunalkhare21@users.noreply.github.com>
2026-01-03 13:13:39 +0530

    fix: use dynamic PORT for production

--- server/index.js
+++ server/index.js
@@ -10,30 +10,22 @@ dotenv.config({ path: path.join(__dirname, ".env") });
 console.log("EMAIL_USER =", process.env.EMAIL_USER);
 console.log("EMAIL_PASS exists =", !!process.env.EMAIL_PASS);
 
-
-
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-// import dotenv from "dotenv";
 import User from "./models/user.js";
 import userRoutes from "./routes/users.js";
-// import newsRoutes from "./routes/news.js";
 import articleRoutes from "./routes/articles.js";
 import authRoutes from "./routes/auth.js";
 import testNewsRoutes from "./routes/news.js";
 
-
 console.log("🔥 THIS index.js FILE IS RUNNING 🔥");
-
-// dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.use("/users", userRoutes);
-// app.use("/news", newsRoutes);
 app.use("/articles", articleRoutes);
 app.use("/auth", authRoutes);
 app.use("/news", testNewsRoutes);
@@ -78,6 +70,9 @@ import "./cron/fetchArticles.cron.js";
 import "./cron/dailyDigest.cron.js";
 
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
+
